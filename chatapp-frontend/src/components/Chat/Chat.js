43f5b556ef2e7f16c79699c693bfb268e2735318
riteishB <riteishy2j@gmail.com>
2020-04-29 11:04:24 -0400

    folder structure

--- chatapp-frontend/src/components/Chat/Chat.js
+++ chatapp-frontend/src/components/Chat/Chat.js
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react'
+import { Redirect } from 'react-router-dom'
 
 export default function Chat(props) {
     const [user, setUser] = useState('')
     const [room, setRoom] = useState('')
+    const [error, setError] = useState()
 
     useEffect(
         () => {
-            setUser(props.location.state.user)
-            setRoom(props.location.state.room)
+            if (props.location.state.user && props.location.state.room) {
+                setUser(props.location.state.user)
+                setRoom(props.location.state.room)
+            } else {
+                setError('Not signed in ')
+            }
         },
         [room, user]
     )
-    return (
-        <div>
-            <h2> Chat page</h2>
-            {user} {room}
-        </div>
-    )
+
+    if (error) {
+        return <Redirect to="/" />
+    } else {
+        return (
+            <div>
+                <h2> Chat page</h2>
+                {user} {room}
+            </div>
+        )
+    }
 }
