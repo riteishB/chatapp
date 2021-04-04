4554d6fffffbd2aae5af3e3bf6c4dcba0d2f2740
Riteish <riteishy2j@gmail.com>
2021-04-04 19:09:49 -0400

    Ui tweaks

--- chatapp-frontend/src/components/Message/Message.js
+++ chatapp-frontend/src/components/Message/Message.js
@@ -11,19 +11,22 @@ import './Message.css'
 
 export default function Message(props) {
     return (
-        <div className="msgWell">
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar>
-                        <PersonIcon />
-                    </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                    secondary={props.message.user}
-                    primary={props.message.message}
-                />
-            </ListItem>
-            <Divider />
+        <div className="message-well">
+                <div className="message-header">
+                    <div className="message-header-icon">
+                        <Avatar>
+                            <PersonIcon />
+                        </Avatar>
+                    </div>
+                    <div className="message-header-text">
+                        <span className="message-user">{props.message.user} </span>
+                        <span className="message-time">{new Date(props.message.time).toLocaleTimeString()} </span>
+                    </div>
+
+                </div>
+                <div className="message-content">
+                    <span className="message">{props.message.message}</span>
+                </div>
         </div>
     )
 }
